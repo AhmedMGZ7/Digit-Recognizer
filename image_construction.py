b17e82d6e4d4b19c89afd286a29438bf65252ca3
AhmedMGZ7 <omagdy837@yahoo.com>
2025-06-11 16:26:09 +0300

    Constructed images for validation after splitting training data

--- image_construction.py
+++ image_construction.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 import os
+from sklearn.model_selection import train_test_split
 
 
 # function to create a folder
@@ -19,9 +20,11 @@ def read_and_create_folders(filepath):
     print("------Creating folders------")
     if 'label' in df.columns:
         create_folder("Data/train/")
+        create_folder("Data/validation/")
         labels = set(np.array(df['label']))
         for label in labels:
             create_folder(f'Data/train/{label}')
+            create_folder(f'Data/validation/{label}')
     else:
         create_folder("Data/test/")
     return df
@@ -41,18 +44,21 @@ def construct_image(df):
             if j % 28 == 0 and j != 0:
                 a.append(row)
                 row = list()
-            row.append(df.loc[i, cols[j]])
+            row.append(images_list.loc[i, cols[j]])
         image = np.array(a, dtype=np.uint8)
         images.append(image)
     return images
 
 #save image in its right label folder
-def save_image(images , labels = None):
+def save_image(images , labels = None , validation = False):
     print("------Saving constructed images------")
     for i in range(len(images)):
         new_image = Image.fromarray(images[i])
         if labels:
-            new_image.save(f'Data/train/{labels[i]}/{i}.png')
+            if validation:
+                new_image.save(f'Data/validation/{labels[i]}/{i}.png')
+            else:
+                new_image.save(f'Data/train/{labels[i]}/{i}.png')
         else:
             new_image.save(f'Data/test/{i}.png')
 
@@ -65,10 +71,26 @@ if __name__ == "__main__":
     else:
         print("Invalid path. Please check the file.")
     print("------Constructing Images------")
-    images = construct_image(df)
+    
     if 'label' in df.columns:
-        labels = list(df["label"])
+
+        df_train , df_validation = train_test_split(df,test_size=0.2)
+
+        # Reset their indices
+        df_train = df_train.reset_index(drop=True)
+        df_validation = df_validation.reset_index(drop=True)
+
+        train_images = construct_image(df_train)
+        train_labels = list(df_train["label"])
+
+        validation_images = construct_image(df_validation)
+        validation_labels = list(df_validation["label"])
+
+        save_image(train_images,train_labels)
+        save_image(validation_images,validation_labels ,validation= True)
+
     else:
+        images = construct_image(df)
         labels = None
-    save_image(images,labels)
+        save_image(images,labels)
 
